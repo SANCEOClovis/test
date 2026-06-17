b6c67785d753a6b5205e51fc942e00044a7c78c0
Clovis Sanceo <clovis.sanceo@insurlytech.com>
2026-06-17 16:17:17 +0200

    test 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestService } from './test.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   template: `
     <h1>Application Angular prête ✅</h1>
     <p>Le front fonctionne correctement.</p>
+    <button (click)="getTest()">Get Data</button>
+    <div>{{ data }}</div>
   `,
   styles: [],
 })
-export class App {}
+export class App {
+  data = '';
+
+  constructor(private testService: TestService) {}
+
+  getTest() {
+    console.log('Appel de la méthode getTest()');
+    this.testService.getData().subscribe(
+      (testData) => {
+        this.data = testData;
+        console.log('Données récupérées avec succès :', this.data);
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des données :', error);
+      },
+    );
+  }
+}
