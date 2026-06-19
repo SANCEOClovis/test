5d09d9160e3998adb1079efd84f7bc9160aacdac
Clovis Sanceo <clovis.sanceo@insurlytech.com>
2026-06-19 09:40:32 +0200

    truc

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,7 +9,6 @@ import { TestService } from './test.service';
     <p>Le front fonctionne correctement.</p>
     <button (click)="getTest()">Get Data</button>
     <div>{{ data }}</div>
-    <p>Le back fonctionne correctement.</p>
   `,
   styles: [],
 })
